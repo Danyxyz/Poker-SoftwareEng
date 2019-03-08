3b3698af39f262b75d0bf94bda6f11a087d11dc9
Danie <Danie@DESKTOP-ODASIVK.edu.ds.fhnw.ch>
2019-03-08 11:13:11 +0100

    First Commit

---
+++ Poker SW/src/poker/version_graphics/model/PokerGameModel.java
@@ -0,0 +1,26 @@
+package poker.version_graphics.model;
+
+import java.util.ArrayList;
+
+import poker.version_graphics.PokerGame;
+
+public class PokerGameModel {
+	private final ArrayList<Player> players = new ArrayList<>();
+	private DeckOfCards deck;
+	
+	public PokerGameModel() {
+		for (int i = 1; i < PokerGame.NUM_PLAYERS+1; i++) {
+			players.add(new Player("Player " + i));
+		}
+		
+		deck = new DeckOfCards();
+	}
+	
+	public Player getPlayer(int i) {
+		return players.get(i);
+	}
+	
+	public DeckOfCards getDeck() {
+		return deck;
+	}
+}
